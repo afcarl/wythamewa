2160677871b5902bfd60cfe8d507cebdcb4a797f
Richard McElreath <rmcelreath@gmail.com>
2015-11-05 13:24:22 +0100

    fix for bad generated quantities
    New version of model template and build code that corrects problem of
    not calculating generated quantities right (ks,kg,kl,kp,kpx).
    Estimation was not affected.

--- R/fit.r
+++ R/fit.r
@@ -37,20 +37,16 @@ ewa_def <- function(model,prior,data,file,pars,start,link=c("logit","logit","log
         lm[[i]] <- concat(inv_link,"(",model[[i]],")")
     }
 
-    # substitutions
-    x <- gsub("!s_i!",lm[[1]],model_template,fixed=TRUE)
-    x <- gsub("!g_i!",lm[[2]],x,fixed=TRUE)
-    x <- gsub("!lambda_i!",lm[[3]],x,fixed=TRUE)
-    x <- gsub("!pay_i!",lm[[4]],x,fixed=TRUE)
-    x <- gsub("!payX_i!",lm[[5]],x,fixed=TRUE)
-    x <- gsub("!priors!",prior,x,fixed=TRUE)
-    model_text <- x
-
     ###################
     # prep data and inits
     if ( missing(data) ) {
         utils::data(WythamUnequal)
         dat <- WythamUnequal
+        dat$N_effects <- N_effects
+        dat$N_veffects <- N_veffects
+        data <- dat
+    } else {
+        dat <- data
         dat$N_effects <- N_effects
         dat$N_veffects <- N_veffects
         data <- dat
@@ -65,8 +61,46 @@ ewa_def <- function(model,prior,data,file,pars,start,link=c("logit","logit","log
         b_age = rep(0,dat$N_effects)
     )
 
+    # build per-individual versions of linear models, for generated quantities
+    # need to replace bird[i] with individual index values j
+    # need to replace age[i] with unique age for individual j in agej[j]
+    # we construct agej here and include in the model
+    lmj <- lm
+    for ( i in 1:length(lmj) ) {
+        lmj[[i]] <- gsub("bird[i]","j",lmj[[i]],fixed=TRUE)
+        lmj[[i]] <- gsub("age[i]","agej[j]",lmj[[i]],fixed=TRUE)
+    }
+    # build agej variable
+    n_birds <- data$N_birds
+    agej <- rep(0,n_birds)
+    for ( j in 1:n_birds ) {
+        ages_found <- unique(data$age[data$bird==j])
+        if ( length(ages_found)>1 ) message(concat("Warning: More than one unique age value found for individual ",j,". Using only first for computing generated quantities."))
+        agej[j] <- ages_found[1]
+    }
+    data$agej <- agej
+
+    # substitutions
+    # linear models and priors
+    x <- gsub("!s_i!",lm[[1]],model_template,fixed=TRUE)
+    x <- gsub("!g_i!",lm[[2]],x,fixed=TRUE)
+    x <- gsub("!lambda_i!",lm[[3]],x,fixed=TRUE)
+    x <- gsub("!pay_i!",lm[[4]],x,fixed=TRUE)
+    x <- gsub("!payX_i!",lm[[5]],x,fixed=TRUE)
+    x <- gsub("!priors!",prior,x,fixed=TRUE)
+    # generated quantities linear models
+    x <- gsub("!s_j!",lmj[[1]],x,fixed=TRUE)
+    x <- gsub("!g_j!",lmj[[2]],x,fixed=TRUE)
+    x <- gsub("!lambda_j!",lmj[[3]],x,fixed=TRUE)
+    x <- gsub("!pay_j!",lmj[[4]],x,fixed=TRUE)
+    x <- gsub("!payX_j!",lmj[[5]],x,fixed=TRUE)
+    # sew it up
+    model_text <- x
+
+    # parameters to sample
     pars <- c( "mu", "b_age", "diff_hi", "sigma_bird", "Rho_bird", "a_bird", "log_lik", "ks", "kg", "kl", "kp" , "kpx" )
 
+    # build result suitable to pass to ewa_fit
     result <- list(
             stan_code = model_text,
             data = data,
@@ -100,13 +134,20 @@ ewa_fit <- function(model,chains=3,cores=3,seed=1,
     for ( i in 1:n_chains ) init_list[[i]] <- model$start
 
     # fit
-    m <- stan( fit=m_prep , data=model$data , init=init_list , iter=iter , warmup=warmup , chains=n_chains , cores=n_cores , pars=model$pars , control=ctrl , refresh=refresh )
-
-    return(m)
+    return( 
+        stan( fit=m_prep , data=model$data , init=init_list , iter=iter , warmup=warmup , chains=n_chains , cores=n_cores , pars=model$pars , control=ctrl , refresh=refresh )
+        )
 }
 
 # test code
 if ( FALSE ) {
+
+# for reading in model tempate and writing out as rda, in order to include in package
+# load model template
+# collapse into single string
+template_raw <- scan("model_EWA_NC_GRETI_templateW.stan",character(),sep="\n")
+template_raw <- paste(template_raw,collapse="\n")
+save(template_raw,file="template_raw.rda")
 
 library(wythamewa)
 
@@ -130,5 +171,34 @@ prior <- "
 mod1 <- ewa_def( model=lms , prior=prior )
 
 m <- ewa_fit( mod1 , warmup=50 , iter=100 , chains=2 )
+
+WAIC(m)
+
+post <- extract.samples(m)
+
+ks <- apply(post$ks,2,median)
+kg <- apply(post$kg,2,median)
+kl <- apply(post$kl,2,median)
+kp <- apply(post$kp,2,median)
+kpx <- apply(post$kpx,2,median)
+
+ks_sd <- apply(post$ks,2,sd)
+kg_sd <- apply(post$kg,2,sd)
+kl_sd <- apply(post$kl,2,sd)
+kp_sd <- apply(post$kp,2,sd)
+kpx_sd <- apply(post$kpx,2,sd)
+
+cav <- Vectorize( col.alpha , "alpha" )
+
+blank(ex=2)
+par(mfrow=c(2,2))
+a <- cav("orange",1-kg_sd/max(kg_sd))
+plot(ks,kg,col=a)
+a <- cav("orange",1-kl_sd/max(kl_sd))
+plot(ks,kl,col=a); abline(h=1,lty=2)
+a <- cav("orange",1-kp_sd/max(kp_sd))
+plot(ks,kp,col=a)
+a <- cav("orange",1-kp_sd/max(kp_sd))
+plot(kp,kpx,col=a)
 
 }
